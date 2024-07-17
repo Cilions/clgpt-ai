c3bf626eaf93a1d0638c4884cc1647d3eb80ce49
cilions <denizkaratov@gmail.com>
2024-07-17 21:53:03 +0500

    next-auth secret

--- auth.ts
+++ auth.ts
@@ -3,4 +3,9 @@ import Google from "next-auth/providers/google"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/",
+  },
+  trustHost: true,
 })
