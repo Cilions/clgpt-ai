4e78336f64b3ec2b85f19f33ef4f9da3d3a38bc7
cilions <denizkaratov@gmail.com>
2024-07-30 00:51:32 +0500

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,19 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getToken } from 'next-auth/jwt'
+import { auth } from './auth'
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ 
-        req: request, 
-        secret: process.env.NEXTAUTH_SECRET || '',
-        salt: process.env.NEXTAUTH_SALT || ''
-    })
+    const session = await auth()
 
-    if (!token && request.nextUrl.pathname.startsWith('/chat')) {
+    if (!session && request.nextUrl.pathname.startsWith('/chat')) {
         return NextResponse.redirect(new URL('/', request.url))
     }
-
-    if (token && request.nextUrl.pathname === '/') {
+    if (session && request.nextUrl.pathname === '/') {
         return NextResponse.redirect(new URL('/chat', request.url))
     }
-
     return NextResponse.next()
 }
 
 export const config = {
-    matcher: ['/', '/chat'],
+  matcher: ['/', '/chat'],
 }
