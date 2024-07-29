cb87ed59f0b59e401771f7b6df27aa83a910cefc
cilions <denizkaratov@gmail.com>
2024-07-30 00:28:23 +0500

    loading

---
+++ app/chat/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+          loading...
+      </div>
+    )
+}
--- middleware.ts
+++ middleware.ts
@@ -3,22 +3,23 @@ import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ 
-    req: request, 
-    secret: process.env.NEXTAUTH_SECRET 
-  })
+    const token = await getToken({ 
+        req: request, 
+        secret: process.env.NEXTAUTH_SECRET || '',
+        salt: process.env.NEXTAUTH_SALT || ''
+    })
 
-  if (!token && request.nextUrl.pathname.startsWith('/chat')) {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
+    if (!token && request.nextUrl.pathname.startsWith('/chat')) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
 
-  if (token && request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/chat', request.url))
-  }
+    if (token && request.nextUrl.pathname === '/') {
+        return NextResponse.redirect(new URL('/chat', request.url))
+    }
 
-  return NextResponse.next()
+    return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/', '/chat'],
+    matcher: ['/', '/chat'],
 }
